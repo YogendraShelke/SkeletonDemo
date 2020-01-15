60ab0003a46d1ac49ec6983d03c6e2dd8fd59683
Yogendra Shelke <yogendra.shelke@citiustech.com>
2020-01-15 19:09:29 +0530

    Initial Commit

---
+++ SkeletonDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonDemo
+//
+//  Created by Yogendra Shelke on 1/15/20.
+//  Copyright © 2020 Yogendra Shelke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
